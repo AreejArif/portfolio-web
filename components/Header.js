6b3b81a7847618f9785e13702a5a593dd317ca8a
Areej Arif <areejarif@192.168.16.102>
2022-10-09 23:52:14 +0500

    structure setup

---
+++ components/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../styles/Header.module.css";
+
+const Header = () => {
+  return <div className={styles.__header___container}>Header</div>;
+};
+
+export default Header;
