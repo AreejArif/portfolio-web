559d5692213a72dded55928b93e21c9fd7e67a31
Areej Arif <areejarif@192.168.16.102>
2022-10-10 12:13:03 +0500

    header

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,17 @@ import React from "react";
 import styles from "../styles/Header.module.css";
 
 const Header = () => {
-  return <div className={styles.__header___container}>Header</div>;
+  return (
+    <div className={styles.__header___container}>
+      <a href="#contact">Contact</a>
+      <a href="#Services">Services</a>
+      <a href="#about">About</a>
+      <a href="#Gallery">Gallery</a>
+      <a class="active" href="#home">
+        Home
+      </a>
+    </div>
+  );
 };
 
 export default Header;
