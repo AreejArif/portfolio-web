ed98614dc465d234fa645ec37af3661fe0763d0f
DevMuneeb <mb380960@gmail.com>
2022-10-10 13:50:39 +0500

    header fixes

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,51 @@
-import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
 import styles from "../styles/Header.module.css";
 
 const Header = () => {
+  const router = useRouter();
+  console.log(router.asPath);
+  const links = [
+    {
+      link: "Home",
+      route: "/",
+    },
+    {
+      link: "Contact",
+      route: "/contact",
+    },
+    {
+      link: "Services",
+      route: "/services",
+    },
+    {
+      link: "About",
+      route: "/about",
+    },
+    {
+      link: "Gallery",
+      route: "/gallery",
+    },
+  ];
+
   return (
     <div className={styles.__header___container}>
-      <a href="#contact">Contact</a>
-      <a href="#Services">Services</a>
-      <a href="#about">About</a>
-      <a href="#Gallery">Gallery</a>
-      <a class="active" href="#home">
-        Home
-      </a>
+      <div className={styles.menu}>
+        {links.map((item, __index) => (
+          <Link key={__index} href={item.route}>
+            <a
+              className={styles.__menu__item}
+              style={{
+                color: router.asPath === item.route ? "#D92929" : "",
+              }}
+            >
+              {" "}
+              {item.link}
+            </a>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <Header />
-      </main>
+      <main className={styles.main}></main>
     </div>
   );
 }
